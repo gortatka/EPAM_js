b2bcd23ac534d69c41b6f72823be35ac43c97695
gortatka <gtatsiana@tut.by>
2021-07-22 12:49:44 +0300

    init: start js

---
+++ operators.js
@@ -0,0 +1,29 @@
+let isTrue = true;
+let str = "string";
+let sum = isTrue + str;
+console.log(sum);
+let number = 15;
+let sum1 = str + 5 + 6;
+console.log(sum1);
+let sum2 = isTrue + number;
+console.log(sum2);
+let multi = isTrue * str;
+console.log(multi);
+let multi1 = str * number;
+console.log(multi1);
+let multi2 = isTrue * number;
+console.log(multi2);
+let div = isTrue / str;
+console.log(div);
+let div1 = str / number;
+console.log(div1);
+let div2 = isTrue / number;
+console.log(div2);
+let a = +"123";
+var b = Number("123");
+console.log(a);
+console.log(b);
+let istrue = +true
+let isFalse = +false
+console.log(istrue);
+console.log(isFalse);
