ab72f2cdda32ea5099a6dbeb2081ef73621517d3
gortatka <gtatsiana@tut.by>
2021-07-26 09:43:14 +0300

    feat: add tryCatch.js

---
+++ tryCatch.js
@@ -0,0 +1,12 @@
+try {
+
+    console.log(a),
+        let a = 3
+    let div = 1 / 0;
+
+} catch (e) {
+
+    console.log('let перед использованием нужно объявить')
+    console.log('на ноль делить нельзя')
+
+}
